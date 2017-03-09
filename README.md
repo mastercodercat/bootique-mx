9cb4d45b816d508173e4f43c0cb970dab2a79243
mastercodercat <mastercoderoy64@gmail.com>
2017-03-09 18:41:36 +0100

    Models and fixtures

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Boutique Air MX Tracking System
 
-Readme content coming soon
+Configure database connection
+
+- In `[project folder]/mxtracking` directory, copy local_settings.py.example to local_settings.py
+- Edit `DATABASES['default']` parameter to connection string of local database (postgres is used as default currently)
+
+To run the project:
+
+- Activate virtual environment
+- Go to project root directory
+- Run `python manage.py makemigrations` to create migrations
+- Run `python manage.py migrate` to migrate database
+- Run `python manage.py runserver` to run project
+
+Initial project configuration:
+
+- Run `python manage.py createsuperuser` to create an admin
+- Login to `http://localhost:8000/admin` to enter django admin
+- Add Site model with site name `localhost`
+- Add Social auth application as Google with api key and secret
+
+Load fixures:
+
+Run all the commands:
+`python manage.py aircrafts.json`
+`python manage.py airframes.json`
+`python manage.py engines.json`
+`python manage.py propellers.json`
--- home/models.py
+++ home/models.py
@@ -37,3 +37,63 @@ class PersonalData(models.Model):
 
     def __unicode__(self):
         return self.first_name + " " + self.last_name
+
+
+class AircraftType(models.Model):
+    type = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        db_table = "aircraft_type"
+
+    def __unicode__(self):
+        return self.type
+
+
+class Aircraft(models.Model):
+    reg = models.CharField(max_length=20)
+    serial = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+
+    type = models.ForeignKey('AircraftType', null=True, blank=False)
+
+    class Meta:
+        db_table = 'aircraft'
+
+    def __unicode__(self):
+        return self.reg
+
+
+class Airframe(models.Model):
+    total_hours = models.IntegerField(default=0, null=True, blank=True)
+    reported_hours = models.IntegerField(default=0, null=True, blank=True)
+    reported_landings = models.IntegerField(default=0, null=True, blank=True)
+    last_inspection_time = models.DateTimeField(blank=True)
+    time_between_inspections = models.IntegerField(default=0, null=True, blank=True)
+
+    aircraft = models.OneToOneField('Aircraft', null=True, blank=False)
+
+    class Meta:
+        db_table = 'airframe'
+
+
+class Engine(models.Model):
+    date = models.DateField(blank=True)
+    engine_hours = models.IntegerField(default=0, null=True, blank=True)
+    engine_cycles = models.IntegerField(default=0, null=True, blank=True)
+    last_hot_section_time = models.DateTimeField(blank=True)
+    last_overhaul_time = models.DateTimeField(blank=True)
+    tbo = models.IntegerField(default=0, null=True, blank=True)
+
+    aircraft = models.ForeignKey('Aircraft', null=True, blank=False)
+
+    class Meta:
+        db_table = 'engine'
+
+
+class Propeller(models.Model):
+    last_inspection_time = models.DateTimeField(blank=True)
+
+    aircraft = models.ForeignKey('Aircraft', null=True, blank=False)
+
+    class Meta:
+        db_table = 'propeller'
