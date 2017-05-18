bc7849d0ad9c82add3464c900900cccad2bd8bb5
mastercodercat <mastercoderoy64@gmail.com>
2017-05-18 04:43:25 +0200

    Fabfile update

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 import os
 from contextlib import contextmanager
-from fabric.api import cd, env, prefix, run, sudo, task
+from fabric.api import cd, env, prefix, run, sudo, task, hide
 
 from fabric_settings import *
 
@@ -34,10 +34,11 @@ def deploy():
     """
     Deploys the latest tag to the production server using docker
     """
-    with cd(PROJECT_ROOT):
-        run('git pull origin master')
-        run('docker-compose build')
-        run('docker-compose up -d')
+    with hide('output'):
+        with cd(PROJECT_ROOT):
+            run('git pull origin master')
+            run('docker-compose build')
+            run('docker-compose up -d')
 
 
 @task
@@ -55,22 +56,23 @@ def bootstrap():
     Setup server environment using docker
     """
 
-    # Install docker
-    sudo('apt-get update')
-    sudo('apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D')
-    sudo("apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'")
-    sudo('apt-get update')
-    run('apt-cache policy docker-engine')
-    sudo('apt-get install -y docker-engine')
+    with hide('output'):
+        # Install docker
+        sudo('apt-get update')
+        sudo('apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D')
+        sudo("apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'")
+        sudo('apt-get update')
+        run('apt-cache policy docker-engine')
+        sudo('apt-get install -y docker-engine')
 
-    # Install docker-compose
-    sudo('curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose')
-    sudo('chmod +x /usr/local/bin/docker-compose')
+        # Install docker-compose
+        sudo('curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose')
+        sudo('chmod +x /usr/local/bin/docker-compose')
 
-    # Create Postgres data folder on host
-    with cd('~'):
-        run('mkdir postgres_data')
+        # Create Postgres data folder on host
+        with cd('~'):
+            run('mkdir postgres_data')
 
-    with cd(PROJECT_ROOT):
-        run('git init')
-        run('git remote add origin ' + GIT_REPO)
+        with cd(PROJECT_ROOT):
+            run('git init')
+            run('git remote add origin ' + GIT_REPO)
